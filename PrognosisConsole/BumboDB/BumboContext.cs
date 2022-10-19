d490a56d57eab9771a5e631c3fff60b10749247f
Dylan Doensen (Laptop) <merelenjoy66@gmail.com>
2022-10-19 15:22:43 +0200

    Project Setup

---
+++ PrognosisConsole/BumboDB/BumboContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace BumboDB;
+
+public sealed class BumboContext : DbContext
+{
+    public BumboContext()
+    {
+            
+    }
+
+    public BumboContext(DbContextOptions<BumboContext> options) : base(options)
+    {
+        Database.EnsureCreated();
+    }
+        
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost\\SQLEXPRESS01;Initial Catalog=BumboDB;Integrated Security=True;");
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+    }
+
+    #region DbSets
+
+    
+
+    #endregion
+}
