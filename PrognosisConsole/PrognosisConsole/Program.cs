ce5108e51238dc28695fdcd645f6808f3809c385
Dylan Doensen (Laptop) <merelenjoy66@gmail.com>
2022-10-20 15:32:40 +0200

    kkr cool

--- PrognosisConsole/PrognosisConsole/Program.cs
+++ PrognosisConsole/PrognosisConsole/Program.cs
@@ -1,3 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using BumboDB;
+using BumboDB.EntityModels;
+using Microsoft.EntityFrameworkCore;
+
+var context = new BumboContext();
+
+Console.Out.WriteLine("Hoeveel klanten worden er verwacht?");
+int klanten = int.Parse(Console.ReadLine());
+
+Console.Out.WriteLine("Hoeveel coli worden er verwacht?");
+int coli = int.Parse(Console.ReadLine());
+
+// Do prognose calculate
+var pInputColi = new PrognoseInput()
+{
+    Datum = DateTime.Now,
+    EenheidType = "Coli",
+    Waarde = coli
+};
+
+var pInputKlanten = new PrognoseInput()
+{
+    Datum = DateTime.Now,
+    EenheidType = "Klanten",
+    Waarde = klanten
+};
